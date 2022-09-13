f8c11d496ced496a0e2c2f833bdbc1602bfac5d3
Ze-Edu <joseeduardoqueiros5@gmail.com>
2022-09-13 11:37:37 -0300

    Api-Mobile_UC11

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import { present } from '@ionic/core/dist/types/utils/overlays';
+import { PostService } from 'src/services/post.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  usuario: string="";
+  senha: string="";
+
+  constructor(
+    private service: PostService,
+    private router: Router,
+    private toastCtrl: ToastController
+  ) { }
+    
+  ngOnInit() {
+  }
+
+  login(){
+    let dados =  {
+      requisicao: 'login',
+      usuario: this.usuario,
+      senha: this.senha
+    }
+    //(dados) é o que esta senda passado para a Api
+    //(data) é o que esta sendo retornado da Api
+    this.service.dadosApi(dados,'usuario.php').subscribe( async data =>{
+      console.log(data)
+      if(data['success']){
+        if(data['result']['nivel']=='admin' || data['result']['nivel']=='caixa')
+            this.router.navigate(['usuarios']);
+      else
+        this.router.navigate(['folder']);
+        const toast = await this.toastCtrl.create({
+          message: "Login efetuado com sucesso",
+          position: 'top',
+          color:'success',
+          duration:2500
+        });
+        toast.present();
+      }else{
+        const toast = await this.toastCtrl.create({
+          message: data['result'],
+          position: 'bottom',
+          color:'danger',
+          duration:2500
+        });
+        toast.present();
+      }
+    });
+  }
+
+}
---
+++ src/services/post.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+  server: string = "http://localhost/api/";
+
+  constructor(private http: HttpClient) {}
+  dadosApi(dados:any, nomeApi: string){
+    const httpOptions = {
+      headers: new HttpHeaders({'content-type':'application/json'})
+    }
+    let url = this.server + nomeApi;
+    return this.http.post(url, JSON.stringify(dados), httpOptions).map(res => res)
+  }
+}
