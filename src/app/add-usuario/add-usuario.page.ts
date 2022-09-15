61ae933c3fa833a17452954ae4aee5aa5927c134
Ze-Edu <joseeduardoqueiros5@gmail.com>
2022-09-15 10:08:34 -0300

    ajustes-addUser

--- src/app/add-usuario/add-usuario.page.ts
+++ src/app/add-usuario/add-usuario.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
+import { PostService } from 'src/services/post.service';
 
 @Component({
   selector: 'app-add-usuario',
@@ -6,10 +8,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-usuario.page.scss'],
 })
 export class AddUsuarioPage implements OnInit {
-id: number=0;
-  constructor() { }
+id: number ;
+nome: string ;
+usuario: string ;
+senha: string ;
+nivel: string ;
+  constructor(
+    private service: PostService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  cadastrar(){
+    return new Promise(res => {
+      let dados = {
+        requisicao: 'add',
+        nome: this.nome,
+        usuario: this.usuario,
+        senha: this.senha,
+        nivel: this.nivel
+      }
+      this.service.dadosApi(dados, "usuario.php").subscribe(data=>{
+        if(data['success']){
+          this.router.navigate(['usuarios']);
+          this.id=null;this.nome="";this.usuario="";this.senha="";this.nivel=""
+        }
+      });
+    });
+  }
+  editar(){
+
   }
 
 }
